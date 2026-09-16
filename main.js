d141ddb5c930718d523bb3fd74ca71a3465c52b5
Gabriel Delmondes <gabrieldelmondes747@gmail.com>
2026-09-16 15:24:14 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const updateTime = () => {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+
+    const timeElement = document.querySelector(".time");
+    timeElement.textContent = `${hours} : ${minutes} : ${seconds}`;
+}
+
+setInterval(updateTime, 1000);
+
+window.addEventListener("load", updateTime);
